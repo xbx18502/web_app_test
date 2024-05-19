bde53aa0dbfcb715dce9adff3a65fed3818ce1ed
peixin xiao <xbx18502@gmail.com>
2024-05-19 22:15:32 +0900

    add .idea

---
+++ front/vue/src/views/AdminView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div style="margin-bottom: 15px">
+      <el-input v-model="input" style="width: 200px" placeholder="请输入内容"></el-input>
+      <el-button type="warning" style="margin-left: 10px">查询</el-button>
+      <el-button type="primary" style="margin-left: 10px">新增</el-button>
+    </div>
+    <div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="日期"></el-table-column>
+        <el-table-column prop="name" label="姓名"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column label="操作">
+          <el-button type="primary">编辑</el-button>
+          <el-button type="danger">删除</el-button>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div style="margin-top: 10px">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[10, 20, 30, 40]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next"
+          :total="400">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+}
+</script>
---
+++ front/vue/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div style="font-size: 18px">欢迎大家跟着武哥，<span style="color: red">手牵手</span>带你一起学习做毕业设计</div>
+    <div style="font-size: 18px; color: red; margin: 10px 0">点赞、投币、收藏、好评 + 关注，支持一波 UP</div>
+    <div style="font-size: 18px">感谢大家</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+}
+</script>
