832173fba9d14ae3d95d2e7d4b39ad0824e15358
peixin xiao <xbx18502@gmail.com>
2024-05-21 01:26:38 +0900

    update back

---
+++ back/springboot/springboot/src/main/java/com/example/entity/Params.java
@@ -0,0 +1,41 @@
+package com.example.entity;
+
+public class Params {
+
+    private String name;
+    private String phone;
+    private Integer pageNum;
+    private Integer pageSize;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
