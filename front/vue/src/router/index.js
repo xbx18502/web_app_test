fec7506d8c8fb1e72adc3160eeedc3737a58e4ec
peixin xiao <xbx18502@gmail.com>
2024-05-21 01:30:05 +0900

    update front

--- front/vue/src/router/index.js
+++ front/vue/src/router/index.js
@@ -1,20 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from "@/views/LoginView.vue";
+import AdminView from "@/views/AdminView.vue";
+import LayoutView from "@/views/Layout.vue";
+import RegisterView from "@/views/RegisterView";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'Login',
+    component: LoginView
   },
   {
-    path: '/admin',
-    name: 'admin',
-    component: () => import('../views/AdminView.vue')
-  }
+    path: '/register',
+    name: 'Register',
+    component: RegisterView
+  },
+  {
+    path: '/',
+    name: 'Layout',
+    component: LayoutView,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        path: 'admin',
+        name: 'admin',
+        component: AdminView
+      }
+    ]
+  },
 ]
 
 const router = new VueRouter({
--- front/vue/src/views/AdminView.vue
+++ front/vue/src/views/AdminView.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     <div style="margin-bottom: 15px">
-      <el-input v-model="input" style="width: 200px" placeholder="请输入内容"></el-input>
-      <el-button type="warning" style="margin-left: 10px">查询</el-button>
-      <el-button type="primary" style="margin-left: 10px">新增</el-button>
+      <el-input v-model="params.name" style="width: 200px" placeholder="请输入姓名"></el-input>
+      <el-input v-model="params.phone" style="width: 200px; margin-left: 5px" placeholder="请输入电话"></el-input>
+      <el-button type="warning" style="margin-left: 10px" @click="findBySearch()">查询</el-button>
+      <el-button type="warning" style="margin-left: 10px" @click="reset()">清空</el-button>
+      <el-button type="primary" style="margin-left: 10px" @click="add()">新增</el-button>
     </div>
     <div>
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期"></el-table-column>
         <el-table-column prop="name" label="姓名"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column prop="sex" label="性别"></el-table-column>
+        <el-table-column prop="age" label="年龄"></el-table-column>
+        <el-table-column prop="phone" label="电话"></el-table-column>
         <el-table-column label="操作">
-          <el-button type="primary">编辑</el-button>
-          <el-button type="danger">删除</el-button>
+          <template slot-scope="scope">
+            <el-button type="primary" @click="edit(scope.row)">编辑</el-button>
+            <el-popconfirm title="确定删除吗？" @confirm="del(scope.row.id)">
+              <el-button slot="reference" type="danger" style="margin-left: 5px">删除</el-button>
+            </el-popconfirm>
+          </template>
         </el-table-column>
       </el-table>
     </div>
@@ -20,38 +27,135 @@
       <el-pagination
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="currentPage4"
-          :page-sizes="[10, 20, 30, 40]"
-          :page-size="100"
+          :current-page="params.pageNum"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="params.pageSize"
           layout="total, sizes, prev, pager, next"
-          :total="400">
+          :total="total">
       </el-pagination>
+    </div>
+    <div>
+      <el-dialog title="请填写信息" :visible.sync="dialogFormVisible" width="30%">
+        <el-form :model="form">
+          <el-form-item label="姓名" label-width="15%">
+            <el-input v-model="form.name" autocomplete="off" style="width: 90%"></el-input>
+          </el-form-item>
+          <el-form-item label="性别" label-width="15%">
+            <el-radio v-model="form.sex" label="男">男</el-radio>
+            <el-radio v-model="form.sex" label="女">女</el-radio>
+          </el-form-item>
+          <el-form-item label="年龄" label-width="15%">
+            <el-input v-model="form.age" autocomplete="off" style="width: 90%"></el-input>
+          </el-form-item>
+          <el-form-item label="电话" label-width="15%">
+            <el-input v-model="form.phone" autocomplete="off" style="width: 90%"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="submit()">确 定</el-button>
+        </div>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
+import request from "@/utils/request";
+
 export default {
   data() {
     return {
-      input: '',
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      params: {
+        name: '',
+        phone: '',
+        pageNum: 1,
+        pageSize: 5
+      },
+      tableData: [],
+      total: 0,
+      dialogFormVisible: false,
+      form: {}
+    }
+  },
+  // 页面加载的时候，做一些事情，在created里面
+  created() {
+    this.findBySearch();
+  },
+  // 定义一些页面上控件出发的事件调用的方法
+  methods: {
+    findBySearch() {
+      request.get("/admin/search", {
+        params: this.params
+      }).then(res => {
+        if (res.code === '0') {
+          this.tableData = res.data.list;
+          this.total = res.data.total;
+        } else {
+          this.$message({
+            message: res.msg,
+            type: 'success'
+          });
+        }
+      })
+    },
+    add() {
+      this.form = {};
+      this.dialogFormVisible = true;
+    },
+    edit(obj) {
+      this.form = obj;
+      this.dialogFormVisible = true;
+    },
+    reset() {
+      this.params = {
+        pageNum: 1,
+        pageSize: 5,
+        name: '',
+        phone: ''
+      }
+      this.findBySearch();
+    },
+    handleSizeChange(pageSize) {
+      this.params.pageSize = pageSize;
+      this.findBySearch();
+    },
+    handleCurrentChange(pageNum) {
+      this.params.pageNum = pageNum;
+      this.findBySearch();
+    },
+    submit() {
+      request.post("/admin", this.form).then(res => {
+        if (res.code === '0') {
+          this.$message({
+            message: '操作成功',
+            type: 'success'
+          });
+          this.dialogFormVisible = false;
+          this.findBySearch();
+        } else {
+          this.$message({
+            message: res.msg,
+            type: 'error'
+          });
+        }
+      })
+    },
+    del(id) {
+      request.delete("/admin/" + id).then(res => {
+        if (res.code === '0') {
+          this.$message({
+            message: '删除成功',
+            type: 'success'
+          });
+          this.findBySearch();
+        } else {
+          this.$message({
+            message: res.msg,
+            type: 'success'
+          });
+        }
+      })
     }
   }
 }
