00a287a95b7b6095d1c02bb2ec2cb2dbbe6dd237
root <root@vultr.guest>
2024-12-13 13:46:56 +0000

    add config for systemd and nginx

---
+++ config/description.sh
@@ -0,0 +1,14 @@
+application.yml for spring boot app
+
+/home/xiao/springboot-project/application.yml
+
+systemd service file for myapp
+
+/etc/systemd/system/myapp.service
+
+
+nginx config file for myapp
+
+/etc/nginx/sites-available/myapp
+
+ln -s /etc/nginx/sites-available/myapp /etc/nginx/sites-enabled/
