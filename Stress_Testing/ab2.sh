fd4f48ffbf82d0bbe378c4fe53f083ce2e186f1a
xbx18502 <xbx18502@gmail.com>
2025-01-29 22:02:47 +0900

    update application.yml

--- Stress_Testing/ab2.sh
+++ Stress_Testing/ab2.sh
@@ -3,8 +3,8 @@
 # 1. Login and get token
 TOKEN=$(curl -s -X POST \
   -H "Content-Type: application/json" \
-  -d '{"username":"xbxh","password":"123","role":"USER"}' \
-  http://localhost:9090/login | grep -o '"token":"[^"]*"' | cut -d'"' -f4)
+  -d '{"username":"xbx3","password":"123","role":"USER"}' \
+  http://localhost:9090/api/login | grep -o '"token":"[^"]*"' | cut -d'"' -f4)
 
 if [ -z "$TOKEN" ]; then
     echo "Failed to get token"
@@ -18,10 +18,10 @@ echo "token: $TOKEN" > headers.txt
 echo "Content-Type: application/json" >> headers.txt
 
 # 3. Run ab with token
-ab -n 1000 -c 100 \
+ab -n 100 -c 100 \
    -H "token: $TOKEN" \
    -H "Content-Type: application/json" \
-   http://127.0.0.1:9090/blog/selectById/18
+   http://127.0.0.1:9090/api/blog/selectById/18
 
 # 4. Cleanup
 rm headers.txt
